53a26e677f3fef6334e2fe6cfed9571fada437f3
goto <gogo8bit@yahoo.co.jp>
2023-01-31 22:48:26 +0900

    table

--- src/musicanalysis/resources/js/components/ExampleComponent.vue
+++ src/musicanalysis/resources/js/components/ExampleComponent.vue
@@ -1,25 +1,32 @@
 <template>
     <div>
-        <h1>Laravel 9 And Vue 3</h1>
-        <v-input type="text" v-model="text"/>
-        <v-btn v-on:click="buttonClicked">クリック</v-btn>
-        <h1>{{ text }}</h1>
+        <el-input v-model="text" placeholder="Please input" /> <br>
+        <el-button type="primary" v-on:click="buttonClicked">Primary</el-button>
+        <el-table :data="tableData" stripe style="width: 100%">
+          <el-table-column prop="name" label="name" width="180" />
+        </el-table>
         <img :src="message">
     </div>
 </template>
 
 <script>
+
 export default {
   data() {
     return {
       message: "",
       text: "",
+      tableData:[],
     };
   },
+  
   methods: {
     async buttonClicked() {
-      const res = await axios.get("/api/hello", {params: {name:this.text}});
-      this.message = res.data;
+      axios.get("/api/hello", {params: {name:this.text}})
+      .then(response => {
+                this.tableData = response.data;
+            })
+      setTimeout('alert(response.data);', 10000);
     },
   },
 };
