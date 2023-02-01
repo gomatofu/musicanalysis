e3d64a5149879619c84610e7e7990f6f5ae2763e
goto <gogo8bit@yahoo.co.jp>
2023-02-01 23:12:28 +0900

    テーブル表示追加

--- src/musicanalysis/resources/js/components/ExampleComponent.vue
+++ src/musicanalysis/resources/js/components/ExampleComponent.vue
@@ -2,8 +2,17 @@
     <div>
         <el-input v-model="text" placeholder="Please input" /> <br>
         <el-button type="primary" v-on:click="buttonClicked">Primary</el-button>
-        <el-table :data="tableData" stripe style="width: 100%">
-          <el-table-column prop="name" label="name" width="180" />
+        <el-table :data="responseData" stripe style="width: 100%">
+          <el-table-column label="image" style="width: 20%">
+            <template slot-scope="scope" class="image-slot">
+              <el-image style="width: 90px; height: 90px" :src="scope.row.image" lazy>
+              </el-image>
+          </template>
+          </el-table-column>
+          <el-table-column prop="name" label="name" style="width: 20%" />
+          <el-table-column prop="genres" label="genres" style="width: 15%" />
+          <el-table-column prop="popularity" label="popularity" style="width: 10%" />
+          <el-table-column prop="url" label="url" style="width: 35%"/>
         </el-table>
         <img :src="message">
     </div>
@@ -16,17 +25,16 @@ export default {
     return {
       message: "",
       text: "",
-      tableData:[],
+      responseData:[],
     };
   },
   
   methods: {
     async buttonClicked() {
-      axios.get("/api/hello", {params: {name:this.text}})
+      await axios.get("/api/hello", {params: {name:this.text}})
       .then(response => {
-                this.tableData = response.data;
+        this.responseData = response.data;
             })
-      setTimeout('alert(response.data);', 10000);
     },
   },
 };
