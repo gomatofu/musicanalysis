48a5f2134b27759978b5faf80a8134dd57148082
goto <gogo8bit@yahoo.co.jp>
2023-02-04 01:04:53 +0900

    pinia導入

--- src/musicanalysis/resources/js/app.js
+++ src/musicanalysis/resources/js/app.js
@@ -1,14 +1,16 @@
 require('./bootstrap');
 
-import { createApp } from 'vue'
-import router from './router.js'
-import ElementPlus from 'element-plus'
-import 'element-plus/dist/index.css'
+import { createApp } from 'vue';
+import router from './router';
+import { createPinia } from "pinia";
+import ElementPlus from 'element-plus';
+import 'element-plus/dist/index.css';
 import App from './app.vue';
 
-const app = createApp({})
+const app = createApp({});
 
 app.component('app-vue', App);
+app.use(createPinia());
 app.use(router);
-app.use(ElementPlus)
+app.use(ElementPlus);
 app.mount('#app');
