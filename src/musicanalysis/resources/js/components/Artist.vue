621f060a3e6fb0e057c7d6be8141519e7fda7f0a
goto <gogo8bit@yahoo.co.jp>
2023-02-10 21:08:00 +0900

    api_get処理

---
+++ src/musicanalysis/resources/js/components/Artist.vue
@@ -0,0 +1,59 @@
+<template>
+    <div>
+        <el-input v-model="text" placeholder="Please input" /> <br>
+        <el-button type="primary" @click="buttonClicked">Primary</el-button>
+        <el-table  v-loading="store.loading" @row-click="handleClick" :data="store.responseData" stripe style="width: 100%">
+          <el-table-column prop='image' label="image" style="width: 20%">
+          </el-table-column>
+          <el-table-column prop="name" label="name" style="width: 20%" />
+          <el-table-column prop="genres" label="genres" style="width: 15%" />
+          <el-table-column prop="popularity" label="popularity" style="width: 10%" />
+          <el-table-column prop="url" label="url" style="width: 35%"/>
+        </el-table>
+    </div>
+</template>
+
+<script>
+import { searchStore } from '@/stores/search'
+
+export default {
+  setup() {
+    const store = searchStore()
+
+    return {
+      store,
+    }
+  },
+  data() {
+    return {
+      message: "",
+      text: "",
+    };
+  },
+  
+  methods: {
+    // ローディング表示
+    loadingStart() {
+      this.loading = true;
+    },
+    loadingStop() {
+      this.loading = false;
+    },
+    buttonClicked() {
+      this.store.artistSearch(this.text);
+    },
+    handleClick(val) {
+      this.$router.push({ path: '/routing', query: { name: val.name} });
+      }
+  },
+};
+</script>
+
+<style>
+body {
+  margin: 0;
+}
+.example-showcase .el-loading-mask {
+  z-index: 9;
+}
+</style>
--- src/musicanalysis/resources/js/components/TestVue.vue
+++ src/musicanalysis/resources/js/components/TestVue.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
-    Hello Vue !!
-    <input type="text" />
+    <a>{{ $route.query.name }}</a>
+    <el-button type="primary" @click="buttonClicked">Primary</el-button>
   </div>
   
 </template>
@@ -13,8 +13,20 @@ export default ({
     console.log("start vue")
 
     onMounted(() => {
-      console.log('start mounted !')
+      alart(this.$route.params.name);
+      alart('aaaaaaaa');
     })
+  },
+  data() {
+    return {
+      text: "",
+    };
+  },
+  methods: {
+    buttonClicked() {
+      alart('aaaaaaaa');
+      alart(this.$route.query.name);
+    }
   },
 })
 </script>
