93b4538a8aab238b46c040c1d3aedbcedcec2dfe
goto <gogo8bit@yahoo.co.jp>
2023-02-15 21:20:59 +0900

    アーティスト画像表示

--- src/musicanalysis/resources/js/components/Album.vue
+++ src/musicanalysis/resources/js/components/Album.vue
@@ -1,32 +1,50 @@
 <template>
   <div>
     <div>
-        <el-table   
-              v-loading="store.loading" 
-              @row-click="handleClick" 
-              :data="store.pagedTableData" 
-              stripe style="width: 100%">
-          <el-table-column prop='image' label="image" style="width: 20%">
-          </el-table-column>
-          <el-table-column prop="name" label="name" style="width: 20%" />
-          <el-table-column prop="release_date" label="release_date" style="width: 15%" />
-          <el-table-column prop="track" label="track" style="width: 10%" />
-          <el-table-column prop="url" label="url" style="width: 35%"/>
-        </el-table>
-        <el-pagination layout="prev, pager, next" :page-size="store.pageSize" :total="store.total" @current-change="setPage">
-     </el-pagination>
+      <el-carousel :interval="2000" type="card" height="200px" width="500px">
+    <el-carousel-item v-for="item in store.tableData" :key="store.tableData">
+      <img class="item__image" :src="item.image" alt="" />
+    </el-carousel-item>
+  </el-carousel>
+  <el-button type="primary" @click="buttonClicked">Primary</el-button>
+      <el-table
+        v-loading="store.loading" 
+        @row-click="handleClick" 
+        :data="store.pagedTableData" 
+        stripe style="width: 100%">
+
+        <el-table-column label="image" :min-width="10" >
+           <template #default="scope">
+             <el-image
+               style="width: 100px; height: 100px"
+               :src="scope.row.image"
+               :zoom-rate="1.2"
+               :initial-index="4"
+               fit="cover"
+             />
+           </template>
+        </el-table-column>
+        <el-table-column prop="name" label="name" :min-width="30" />
+        <el-table-column prop="release_date" label="release_date" :min-width="15" />
+        <el-table-column prop="track" label="track" :min-width="15" />
+        <el-table-column label="url" :min-width="30">
+          <template #default="scope">
+            <el-link :href="scope.row.url">{{scope.row.url}}</el-link>
+           </template>
+        </el-table-column>
+      </el-table>
+      <el-pagination layout="prev, pager, next" :page-size="store.pageSize" :total="store.total" @current-change="setPage">
+      </el-pagination>
     </div>
   </div>
-  
 </template>
 
 <script>
 import { albumStore } from '@/stores/album'
 export default ({
-  name: "Albums",
+  name: "Album",
   setup() {
-    const store = albumStore()
-
+    const store = albumStore();
     return {
       store,
     }
@@ -46,3 +64,14 @@ export default ({
   }
 })
 </script>
+
+<style scoped>
+.item__image{
+  color: #475669;
+  opacity: 0.75;
+  line-height: 200px;
+  margin: 0;
+  text-align: center;
+  height: 200px;
+}
+</style>
--- src/musicanalysis/resources/js/components/Artist.vue
+++ src/musicanalysis/resources/js/components/Artist.vue
@@ -3,12 +3,25 @@
         <el-input v-model="text" placeholder="Please input" /> <br>
         <el-button type="primary" @click="buttonClicked">Primary</el-button>
         <el-table  v-loading="store.loading" @row-click="handleClick" :data="store.responseData" stripe style="width: 100%">
-          <el-table-column prop='image' label="image" style="width: 20%">
-          </el-table-column>
+          <el-table-column label="image" width="180">
+           <template #default="scope">
+             <el-image
+               style="width: 100px; height: 100px"
+               :src="scope.row.image"
+               :zoom-rate="1.2"
+               :initial-index="4"
+               fit="cover"
+             />
+           </template>
+        </el-table-column>
           <el-table-column prop="name" label="name" style="width: 20%" />
           <el-table-column prop="genres" label="genres" style="width: 15%" />
           <el-table-column prop="popularity" label="popularity" style="width: 10%" />
-          <el-table-column prop="url" label="url" style="width: 35%"/>
+          <el-table-column label="url" style="width: 35%">
+          <template #default="scope">
+            <el-link :href="scope.row.url">{{scope.row.url}}</el-link>
+           </template>
+        </el-table-column>
         </el-table>
     </div>
 </template>
