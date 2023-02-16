8619f77d4fe30a40792d26fc0431e0fae508980e
goto <gogo8bit@yahoo.co.jp>
2023-02-17 00:53:19 +0900

    stores内でrouterを使用可能に修正

--- src/musicanalysis/resources/js/app.js
+++ src/musicanalysis/resources/js/app.js
@@ -1,16 +1,17 @@
 require('./bootstrap');
 
-import { createApp } from 'vue';
+import { createApp , markRaw } from 'vue';
 import router from './router';
 import { createPinia } from "pinia";
 import ElementPlus from 'element-plus';
 import 'element-plus/dist/index.css';
-import { createPersistedState } from "pinia-plugin-persistedstate";
 import App from '@/app.vue';
 
 const app = createApp({});
 const pinia = createPinia(); 
-pinia.use(createPersistedState());
+pinia.use(({ store }) => {
+    store.$router = markRaw(router)
+  });
 
 app.component('app-vue', App);
 app.use(pinia);
